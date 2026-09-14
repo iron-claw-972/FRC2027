83c41727f34ac56b8a2ba492c00ae7ecb2f7be9f
Ethan Mortensen <ethanmortensen20@gmail.com>
2026-09-14 15:25:43 -0700

    motor.spin
    fixed createVersionFile

--- src/main/java/frc/robot/util/BuildData.java
+++ src/main/java/frc/robot/util/BuildData.java
@@ -1,17 +1,19 @@
 package frc.robot.util;
 
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildData {
-  public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "FRC2027";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "f59c39e0630109646b452869f6842ebcc9f5d923";
-  public static final String GIT_DATE = "2026-08-22T16:10:21Z";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-08-23T16:49:48Z";
-  public static final long BUILD_UNIX_TIME = 1787503788150L;
-  public static final int DIRTY = 0;
+	public static final String MAVEN_GROUP = "";
+	public static final String MAVEN_NAME = "FRC2027";
+	public static final String VERSION = "unspecified";
+	public static final int GIT_REVISION = 33;
+	public static final String GIT_SHA = "3e6e1cab6ca034cca8f893bf4f27a0e277b3ede6";
+	public static final String GIT_DATE = "2026-09-14T22:07:49Z";
+	public static final String GIT_BRANCH = "new_motor_spin";
+	public static final String BUILD_DATE = "2026-09-14T22:24:52Z";
+	public static final long BUILD_UNIX_TIME = 1789424692029L;
+	public static final int DIRTY = 1;
 
-  private BuildData() {}
+	private BuildData(){}
 }
