d49319bb4f7b86c1a4e7f2884fad909335c810ce
Ethan Mortensen <ethanmortensen20@gmail.com>
2026-09-14 15:47:16 -0700

    killing daemons

--- src/main/java/frc/robot/util/BuildData.java
+++ src/main/java/frc/robot/util/BuildData.java
@@ -7,12 +7,12 @@ public final class BuildData {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "FRC2027";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 35;
-	public static final String GIT_SHA = "3508744f1bc3e4cb18be44ff17ec9a443238cb3d";
-	public static final String GIT_DATE = "2026-09-14T22:30:04Z";
+	public static final int GIT_REVISION = 36;
+	public static final String GIT_SHA = "509af14168df1f186e9bcb5a426cc587485c4231";
+	public static final String GIT_DATE = "2026-09-14T22:42:52Z";
 	public static final String GIT_BRANCH = "new_motor_spin";
-	public static final String BUILD_DATE = "2026-09-14T22:42:08Z";
-	public static final long BUILD_UNIX_TIME = 1789425728908L;
+	public static final String BUILD_DATE = "2026-09-14T22:46:47Z";
+	public static final long BUILD_UNIX_TIME = 1789426007830L;
 	public static final int DIRTY = 0;
 
 	private BuildData(){}
